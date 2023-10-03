e879e86dc2fdea38432528de3e4905a415e4aa62
ajmal <ajmalmirshaa2004@gmail.com>
2023-10-03 11:37:28 +0530

    Three.js sphere rotation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import * as THREE from "three";
-import './App.css';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import "./App.css";
 
 function App() {
   const canvasRef = useRef(null);
@@ -16,18 +17,33 @@ function App() {
     scene.add(mesh);
 
     // setup camera
-    const camera = new THREE.PerspectiveCamera(45, 800 / 600);
+    const camera = new THREE.PerspectiveCamera(45, 800 / 600, 0.1, 100);
     camera.position.z = 20;
 
-    // set light
-    const light = new THREE.PointLight(0xffffff, 1, 100);
-    light.position.set(0, 10, 10); // Move the light
-    scene.add(light);
+    // // set light
+    // const light = new THREE.PointLight(0xffffff, 1, 100);
+    // light.position.set(0, 10, 10); // Move the light
+    // scene.add(light);
+
+    // Add a directional light to the scene to cast shadows on the sphere.
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+    directionalLight.position.set(10, 10, 10);
+    scene.add(directionalLight);
 
     // render
     const canvas = canvasRef.current;
     const renderer = new THREE.WebGLRenderer({ canvas });
     renderer.setSize(800, 600);
+
+    // controlls
+
+    const controlls = new OrbitControls(camera, canvas);
+    controlls.enableDamping = true
+    controlls.enableZoom = false
+    controlls.autoRotate = true
+    controlls.autoRotateSpeed = 15
+    controlls.enableRotate = true
+
 
     const animate = () => {
       // Render the scene continuously
@@ -36,14 +52,22 @@ function App() {
     };
 
     animate();
+
+    const loop = () => {
+      // mesh.position.x += 0.02
+      controlls.update()
+      renderer.render(scene, camera);
+      window.requestAnimationFrame(loop);
+    };
+    loop();
   }, []);
 
   return (
     <>
       hello world
-      <canvas className='display' ref={canvasRef}></canvas>
+      <canvas className="display" ref={canvasRef}></canvas>
     </>
-  )
+  );
 }
 
 export default App;
