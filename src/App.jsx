00c6de9929e4e1a5c7e99a7c90c5817858bf2ec2
ajmal <ajmalmirshaa2004@gmail.com>
2023-10-24 13:49:03 +0530

    new updations

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ function App() {
       <div className="div">page 1</div>
       <div className="div" style={{backgroundColor:'black',color:'white'}} >page 2</div>
       <div className="div">page 3</div>
+      <div className="div">page 4</div>
+      <div className="div">page 5</div>
+      <div className="div">page 6</div>
     </>
   );
 }
